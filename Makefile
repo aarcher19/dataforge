0d06c41647de41255601ce86853a8a90ae618b00
aarcher19 <andrewjay.archer@gmail.com>
2025-03-14 15:06:36 -0400

    updated test structure and created generic util test file

--- Makefile
+++ Makefile
@@ -6,9 +6,10 @@ INCLUDES_DIR = includes
 SRC_DIR = src
 BUILD_DIR = build
 LIB_DIR = lib
+UTIL_DIR = $(SRC_DIR)/utils
 
-SRC_FILES = $(wildcard $(SRC_DIR)/*.c)
-OBJ_FILES = $(SRC_FILES:$(SRC_DIR)/%.c=$(BUILD_DIR)/%.o)
+SRC_FILES = $(wildcard $(SRC_DIR)/*.c) $(wildcard $(UTIL_DIR)/*.c)
+OBJ_FILES = $(patsubst $(SRC_DIR)/%.c, $(BUILD_DIR)/%.o, $(SRC_FILES))
 
 LIB_NAME = $(LIB_DIR)/libdataforge.so
 
@@ -17,11 +18,17 @@ all: $(LIB_NAME)
 $(LIB_NAME): $(OBJ_FILES)
 	$(CC) $(LDFLAGS) $(OBJ_FILES) -o $(LIB_NAME)
 
-$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c | $(BUILD_DIR)
 	$(CC) $(CFLAGS) -I$(INCLUDES_DIR) -c $< -o $@
 
+$(BUILD_DIR)/%.o: $(UTIL_DIR)/%.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) -I$(INCLUDES_DIR) -c $< -o $@
+
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+
 clean:
-	rm -rf $(BUILD_DIR)/*.o $(LIB_DIR)/*.a
+	rm -rf $(BUILD_DIR)/*.o $(LIB_NAME)
 
 INSTALL_INCLUDE_DIR = /usr/local/include/dataforge  
 INSTALL_LIB_DIR = /usr/local/lib
---
+++ test/src/utils/test_utils.c
@@ -0,0 +1,36 @@
+#include <criterion/alloc.h>
+#include <criterion/criterion.h>
+#include <criterion/internal/assert.h>
+#include <criterion/internal/test.h>
+#include <criterion/logging.h>
+#include <dataforge/df_array.h>
+#include <dataforge/df_iterator.h>
+#include <dataforge/df_utils.h>
+
+// Helper funcs
+void *double_value2(void *element) {
+    *(int *)element *= 2;
+    return element;
+}
+
+// map tests
+
+Test(generic_utils, map_df_array) {
+  DfArray *array = DfArray_Create(sizeof(int), 3);
+  int nums[] = {10, 20, 30};
+  for(int i = 0; i < 3; i++) {
+    DfArray_Push(array, &nums[i]);
+  }
+  Iterator it = DfArray_Iterator_Create(array);
+  DfArray *modified_array = (DfArray *)DfMap(&it, double_value2);
+  Iterator modified_it = DfArray_Iterator_Create(modified_array);
+  while(it.has_next(&it)){
+    cr_assert(*(int *)it.next(&it) * 2 == *(int *)modified_it.next(&modified_it), "Expected modified value to be double original value");
+  }
+  Iterator_Destroy(&it);
+  Iterator_Destroy(&modified_it);
+  DfArray_Destroy(array);
+  DfArray_Destroy(modified_array);
+}
+
+
