7ff2ecd7ef1c3b3d542823cb56a5da74c48891d4
aarcher19 <andrewjay.archer@gmail.com>
2025-03-25 17:18:07 -0400

    create and destroy funcs for list

--- includes/df_list.h
+++ includes/df_list.h
@@ -1,15 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdio.h>
-
 // Single Linked
 typedef struct DfList_S DfList_S;
 
 typedef struct DfList_S_Node DfList_S_Node;
 
-DfList_S *DfList_S_Create(size_t elem_size);
+DfList_S *DfList_S_Create();
 
-void DfList_S_Destroy(DfList_S *list);
+void DfList_S_Destroy(DfList_S *list, void (*cleanup)(void *element));
 
 #endif
